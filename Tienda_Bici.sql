c57be3c6e203d23f6951d9b1a66eac33e1590b8a
PabloRuiz1312 <pruican1312@g.educaand.es>
2023-04-01 11:04:57 +0200

    Subida del proyecto y creacion de la clase cliente

---
+++ Tienda_Bici.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF exists tienda_bici;
+CREATE DATABASE tienda_bici;
+
+CREATE TABLE Cliente(
+ID INT NOT NULL,
+Nombre VARCHAR(50),
+Edad INT,
+Genero VARCHAR (10),
+PRIMARY KEY (ID)
+);
+
+INSERT INTO Cliente (ID,Nombre,Edad,Genero) VALUES (1,'Pablo Ruiz',19,'Hombre');
+INSERT INTO Cliente (ID,Nombre,Edad,Genero) VALUES (2,'Miguel Rios',20,'Hombre');
+INSERT INTO Cliente (ID,Nombre,Edad,Genero) VALUES (3,'Irene Ruiz',16,'Mujer');
+
+CREATE TABLE Bicicleta(
+ID INT NOT NULL,
+Marca VARCHAR(60),
+Num_Rueda INT NOT NULL,
+Tipo_Bici VARCHAR(30),
+Genero CHAR(1),
+PRIMARY KEY(ID)
+);
+
+INSERT INTO Bicicleta (ID,Marca,Num_Rueda,Tipo_Bici,Genero) VALUES (1,'CONWAY',29,'Rigida','H');
+INSERT INTO Bicicleta (ID,Marca,Num_Rueda,Tipo_Bici,Genero) VALUES (2,'SCOTT',26,'Doble','H');
+INSERT INTO Bicicleta (ID,Marca,Num_Rueda,Tipo_Bici,Genero) VALUES (3,'CABE MOTOR',26,'Rigida','M');
+INSERT INTO Bicicleta (ID,Marca,Num_Rueda,Tipo_Bici,Genero) VALUES (4,'TOTAL',20,'BMX','T');
+
+CREATE TABLE Registro_Compra(
+ID_Cliente INT NOT NULL,
+ID_Bicicleta INT NOT NULL,
+ID_Registro INT NOT NULL,
+Fecha_Compra DATE,
+PRIMARY KEY (ID_Registro),
+FOREIGN KEY (ID_Cliente) REFERENCES Cliente (ID) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY (ID_Bicicleta) REFERENCES Bicicleta (ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
---
+++ src/main/java/es/iesjandula/tienda_bici/clasesbase/Cliente.java
@@ -0,0 +1,130 @@
+package es.iesjandula.tienda_bici.clasesbase;
+/**
+ * 
+ * @author Pablo Ruiz Canovas
+ * This class takes care of save the information of the client 
+ */
+public class Cliente {
+	//This attribute register the identifier of the client 
+	private int id;
+	//This attribute register the name of the client
+	private String name;
+	//This attribute register the age of the client
+	private int age;
+	//This attribute rergister the gender of the client
+	private String gender;
+	
+	/**
+	 * Constructor
+	 * @param id identifier of the client
+	 * @param name name of the client
+	 * @param age age of the client
+	 * @param gender gender of the client
+	 * If one of this attributes (except ID) would be null the method fixAttributes set a default value
+	 */
+	public Cliente (int id,String name,int age,String gender)
+	{
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.fixAttributes();
+	}
+	
+	//Getters and Setters
+	/**
+	 * 
+	 * @return
+	 * This getter return the value of the identifier of the client
+	 */
+	public int getID()
+	{
+		return this.id;
+	}
+	/**
+	 * 
+	 * @return
+	 * This getter return the value of the name of the client
+	 */
+	public String getName()
+	{
+		return this.name;
+	}
+	/**
+	 * 
+	 * @param name
+	 * This setter allows change the value of the name
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	/**
+	 * 
+	 * @return
+	 * This getter return the value of the age of the client
+	 */
+	public int getAge()
+	{
+		return this.age;
+	}
+	/**
+	 * 
+	 * @param age
+	 * This setter allows change the value of the age of the client
+	 */
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	/**
+	 * 
+	 * @return
+	 * This getter return the value of the gender of the client
+	 */
+	public String getGender()
+	{
+		return this.gender;
+	}
+	/**
+	 * 
+	 * @param gender
+	 * This setter allows change the value of the gender of the client
+	 */
+	public void setGender(String gender)
+	{
+		this.gender = gender;
+	}
+	/**
+	 * This method set to default the attributes value if the user set his values null
+	 */
+	private void fixAttributes()
+	{
+		if(this.name.isEmpty())
+		{
+			this.name = "Without name";
+		}
+		if(this.age<=0)
+		{
+			System.out.println("Negative age, set default");
+			this.age = 18;
+		}
+		if(this.gender.isEmpty())
+		{
+			this.gender = "Other";
+		}
+	}
+	@Override
+	/**
+	 * @return 
+	 * This method returns the value of all attributes
+	 */
+	public String toString()
+	{
+		return "Client "+this.id+"{ name="+this.name+" age="+this.age+" gender="+this.gender;
+	}
+	
+	
+	
+	
+}
